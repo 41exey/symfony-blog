c63505a65fd5d22a2ec63dc3e2d92d7f8496ae44
clewd <c1ewd@protonmail.com>
2018-10-02 14:40:56 +0700

    Added Author.php file

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @author c1ewd <c1ewd@protonmail.com>
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/", name="homepage")
      */
-    public function index()
+    public function indexAction()
     {
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
